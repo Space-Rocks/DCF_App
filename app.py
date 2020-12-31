1af1d7873c3444fa3c8ce06fd17907f80682a9e5
space-rocks <james.cassanelli@gmail.com>
2020-12-30 18:43:19 -0600

    Added cash flow projection function.

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 # Import app modules
-from data import get_cash_flow_data
+from data import get_cash_flow_data, project_free_cash_flow
 import matplotlib.pyplot as plt
 # from db import
 
 # Placeholders
-ticker = 'AAPL'
+ticker = 'CVX'
 
 # Execute app
 if __name__ == "__main__":
-    years,fcf = get_cash_flow_data(ticker)
+    years,free_cash_flow = get_cash_flow_data(ticker)
+    fit_years,fit_data,projected_years,projected_free_cash_flow = project_free_cash_flow(years,free_cash_flow,5,2)
 
     plt.figure(1)
-    plt.plot(years, fcf, 'ok')
+    plt.plot(years, free_cash_flow, 'ok', label='Historical')
+    plt.plot(fit_years, fit_data, '-k', label='Fit')
+    plt.plot(projected_years, projected_free_cash_flow, '-r', label='Projection')
     plt.title(ticker)
     plt.xlabel('Year')
     plt.ylabel('Free Cash Flow ($)')
+    plt.legend()
     plt.show()
 
 
--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 from yahoofinancials import YahooFinancials
+import numpy as np
 
 def get_cash_flow_data(ticker):
 
@@ -19,5 +20,27 @@ def get_cash_flow_data(ticker):
 
     return(years,free_cash_flow)
 
+def project_free_cash_flow(years,free_cash_flow,years_forward,method):
 
+    """
+    Project free cash flow data forward by given number of years following prescribed method.
 
+    Keyword arguments:
+    year -- input list of years for free cash flow data to project
+    free_cash_flow -- list of free cash flow data on which to base projection
+    years_forward -- the number of years to forward project free cash flow
+    method -- the method by which to fit the input free cash flow data and project forward (polynomial degree)
+    """
+
+    # Create list of years over which to project free cash flow
+    projected_years = np.linspace(max(years),max(years)+years_forward,years_forward*10)
+    
+    # Generate fit to input data and calculate projected free cash flow
+    if type(method) == int:
+        fit = np.polyfit(years,free_cash_flow,method)
+        fit_function = np.poly1d(fit)
+        fit_years = np.linspace(min(years),max(years),(max(years)-min(years))*10)
+        fit_data = fit_function(fit_years)
+        projected_free_cash_flow = fit_function(projected_years)
+    
+    return(fit_years,fit_data,projected_years,projected_free_cash_flow)
