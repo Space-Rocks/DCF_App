87a7fc9929ed16695a720e9c0b2dd2759576d933
space-rocks <james.cassanelli@gmail.com>
2020-12-31 11:14:36 -0600

    Incorporated db functionality.

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
 # Import app modules
-from data import get_cash_flow_data, project_free_cash_flow
+import data
+import db
 import matplotlib.pyplot as plt
-# from db import
 
 # Placeholders
 ticker = 'CVX'
+years_forward = 5
+method = 1
 
-# Execute app
-if __name__ == "__main__":
-    years,free_cash_flow = get_cash_flow_data(ticker)
-    fit_years,fit_data,projected_years,projected_free_cash_flow = project_free_cash_flow(years,free_cash_flow,5,2)
+# Define main app function
+def main():
+    """
+    Main app function
+    """
 
+    # If data not in databse fetch and insert
+    if db.retrieve_one(ticker) == None:
+
+        # Retrieve data from web using data module
+        years,free_cash_flow = data.get_cash_flow_data(ticker)
+
+        # Create document and insert into NoSQL database
+        document = db.create_document(ticker,years,free_cash_flow)
+        db.insert_one(document)
+
+    # Else retrieve from database
+    else:
+        
+        # Retrieve data from NoSQL database
+        db_data = db.retrieve_all(ticker)
+        years = db_data[0]['years']
+        free_cash_flow = db_data[0]['free_cash_flow']
+    
+    # Fit historical data with desired method and project forward given number of years
+    fit_years,fit_data,projected_years,projected_free_cash_flow = data.project_free_cash_flow(years,free_cash_flow,years_forward,method) 
+
+    # Plot data for visualization
     plt.figure(1)
     plt.plot(years, free_cash_flow, 'ok', label='Historical')
     plt.plot(fit_years, fit_data, '-k', label='Fit')
@@ -21,5 +46,9 @@ if __name__ == "__main__":
     plt.legend()
     plt.show()
 
+# Execute app
+if __name__ == "__main__":
+    main()
 
-
+# Placeholder for testing 
+foo = 0
--- db.py
+++ db.py
@@ -1,43 +1,44 @@
-#Import libraries and packages
+# Import libraries and packages
 import datetime
 import pymongo
 from pymongo import MongoClient
 
-#Assign client, database, and collection
-client = MongoClient() #Client
-db = client.StockFinancialData #Database
-Stocks = db.Stocks #Collection
+# Assign client, database, and collection
+if __name__ == "db":
+        client = MongoClient() #Client
+        db = client.StockFinancialData #Database
+        Stocks = db.Stocks #Collection
 
-#Create document
-def create_document(ticker,year,free_cash_flow):
-        Stock = {"ticker": "AAPL", 
-                "year": year, 
+# Create document
+def create_document(ticker,years,free_cash_flow):
+        Stock = {"ticker": ticker, 
+                "year": years, 
                 "free_cash_flow": free_cash_flow}
         return(Stock)
 
-#Insert one document
+# Insert one document
 def insert_one(stock):
         stock_id = Stocks.insert_one(stock).inserted_id
         return(stock_id)
 
-#Update stock data
+# Update stock data
 def update_stock(ticker,year,free_cash_flow):
         stock = {"ticker": ticker}
         updates = {"$set": {"year": year, "free_cash_flow": free_cash_flow}}
         Stocks.update_one(stock, updates)
 
-#Append new stock data
+# Append new stock data
 def append_stock(ticker,year,free_cash_flow):
         stock = {"ticker": ticker}
         pushes = {"$push": {"year": year, "free_cash_flow": free_cash_flow}}
         Stocks.update_one(stock, pushes)
 
-#Retrieve one document
+# Retrieve one document
 def retrieve_one(ticker):
         data = Stocks.find_one({'ticker': ticker})
         return(data)
 
-#Retrieve all documents
+# Retrieve all documents
 def retrieve_all(ticker):
         data = Stocks.find({'ticker': ticker})
         output = []
